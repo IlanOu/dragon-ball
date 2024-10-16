e85278519c5b79070bf6264bdf8f7e77b85b9d4e
IlanOu <71207643+IlanOu@users.noreply.github.com>
2024-10-16 16:22:43 +0200

    Add : ScriptableObject for technics

---
+++ Assets/Scripts/Warriors/Technics/TechnicDatabase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "TechnicDatabase", menuName = "Game/Technic Database")]
+public class TechnicDatabase : ScriptableObject
+{
+    [System.Serializable]
+    public class TechnicInfo
+    {
+        public string name;
+        public TechnicType technicType;
+        public RaceType raceType;
+        public GameObject prefabTechnic;
+    }
+
+    public List<TechnicInfo> Technics;
+}
---
+++ Assets/Scripts/Warriors/Technics/TechnicType.cs
@@ -0,0 +1,6 @@
+
+public enum TechnicType
+{
+    Kamehameha,
+    Rasengan,
+}
